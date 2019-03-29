2a91c285ed08682c2c21062d20ea4f5aaa4e9f06
John <christopherjohnosullivan@gmail.com>
2019-03-29 13:41:30 +0000

    render function added

--- canvas.js
+++ canvas.js
@@ -2,3 +2,21 @@ var canvas = document.querySelector('canvas');
 var context = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 400;
+
+// To test render() create objects[] array and pass to render()
+
+
+
+function render(objects){
+    objects.forEach(function(object){
+        if (object[3] == true)
+        {
+            context.drawImage(object[2], object[0], object[1]);
+            console.log("image");
+        }
+        else if (object[3] == false){
+            context.fillText(object[2], object[0], object[1]);
+            console.log("text");
+        }
+    })
+}
