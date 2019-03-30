610cf8032d253495e46d5429fefd4eeaf606519c
John <christopherjohnosullivan@gmail.com>
2019-03-30 11:33:23 +0000

    Added example htmlTag scene

--- canvas.js
+++ canvas.js
@@ -3,19 +3,13 @@ var context = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 400;
 
-var sprites = document.getElementById("sprites");
-var frames = sprites.getElementsByTagName("img");
 var objects = [];
 
-// test objects
-for (var i = 0; i < frames.length; i++) {
-    var x = Math.random() * 600;
-    var y = Math.random() * 400;
-    objects.push([x, y, frames[i]]);
-}
-objects.push([150, 170, "Text"]);
-render(objects);
+function load(){
+    var sprites = document.getElementById("sprites");
+    var frames = sprites.getElementsByTagName("img");
 
+}
 
 function clipStart(){
     // Initiate Clip
