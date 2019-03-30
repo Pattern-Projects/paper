0c5f46e9ec6343995a907c2f2c6918a88844da76
John <christopherjohnosullivan@gmail.com>
2019-03-30 23:52:18 +0000

    Bug-fix: One scene being added to scenes

--- canvas.js
+++ canvas.js
@@ -21,29 +21,28 @@ var fire = false;
 function load() { //Loads in items from HTML
     // Collect all info from HTML
     var HTMLscenes = document.getElementsByTagName("scene");
-    for (var i = 0; i < HTMLscenes.length; i++) {
+    for (var i = 0; i < HTMLscenes.length; i++) {                       //Every scene in document
         var scene = {};
         var beatsArray = [];
         scene.type = HTMLscenes[i].getAttribute("type");
         var beats = HTMLscenes[i].getElementsByTagName("beat");
-        for (var i = 0; i < beats.length; i++) {
+        for (var j = 0; j < beats.length; j++) {                        //Every beat in scene
             var beat = {};
-            var objectsArray = []; 
-            beat.spriteLength = beats[i].getAttribute("spriteLength");
-            var objects = beats[i].getElementsByTagName("object");
-            for (var j = 0; j < objects.length; j++) {
+            var objectsArray = [];
+            beat.spriteLength = beats[j].getAttribute("spriteLength");
+            var objects = beats[j].getElementsByTagName("object");
+            for (var k = 0; k < objects.length; k++) {                  //Every object in beat
                 var object = {};
-                object.x = parseInt(objects[j].getAttribute("x"), 16);
-                object.y = parseInt(objects[j].getAttribute("y"), 16);
-                object.name = objects[j].getAttribute("name");
+                object.x = parseInt(objects[k].getAttribute("x"), 16);
+                object.y = parseInt(objects[k].getAttribute("y"), 16);
+                object.name = objects[k].getAttribute("name");
 
-                var sprites = objects[j].getElementsByTagName("img");
+                var sprites = objects[k].getElementsByTagName("img");   //Every sprite in object
                 object.sprites = sprites;
                 objectsArray.push(object);
             }
             beat.objects = objectsArray;
             beatsArray.push(beat);
-            
         }
         scene.beats = beatsArray;
         scenes.push(scene);
@@ -63,57 +62,58 @@ var sprite = 0;
 async function action() { //Runs the scene
     var renders = [];
     var beats = scene.beats;
-    var thisBeat = beats[beat];     //Issue retrieving beat at beats[beats.length] Issue exists in load
+    var thisBeat = beats[beat]; //Issue retrieving beat at beats[beats.length] Issue exists in load
     var objects = thisBeat.objects;
-    await getInput();
-    
-    
 
+    await getInput();
+    //Different inputs responses depending on beat
+    //Here..
+
+    //Iterate sprites, loop when too high
     sprite++;
     if (sprite >= thisBeat.spriteLength) {
         sprite = 0;
     }
-    
+
     //Sprite index
     objects.forEach(function(object) {
-        var si = parseInt(sprite / (thisBeat.spriteLength / object.sprites.length), 16);
-        if(object.sprites[si].src != "")
-        {
-        var item = [];
-        item.push(object.sprites[si]);
-        item.push(object.x);
-        item.push(object.y);
-        item.push(object.name);
-        renders.push(item);
+        var spriteIndex = parseInt(sprite / (thisBeat.spriteLength / object.sprites.length), 16);
+        if (object.sprites[spriteIndex].src != "") {
+            var item = [];
+            item.push(object.sprites[spriteIndex]);
+            item.push(object.x);
+            item.push(object.y);
+            item.push(object.name);
+            renders.push(item);
         }
-        else{
+        else {
             beat++;
             sprite = 0;
         }
     });
     render(renders);
 
-    //Need to detect button press
+    //detect button press
     //When pressed advance to next beat
-    function getInput(){
-    
-    if (!input[38]){
-        fire = false;
-    }
-    
-    if (input[38]){
-        if (!fire){
-        fire = true;
-        // Key Press Detected
-        beat++;
-        sprite = 0;
+    function getInput() {
+
+        if (!input[39]) {
+            fire = false;
         }
-    }
-    
+
+        if (input[39]) {
+            if (!fire) {
+                fire = true;
+                // Key Press Detected
+                beat++;
+                sprite = 0;
+            }
+        }
+
     }
 
     //Loop beats when too high    
-    if (beat >= beats.length){
+    if (beat >= beats.length) {
         beat = 0;
     }
 
