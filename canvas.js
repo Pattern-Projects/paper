64da6be4dc8f4e02d92da6afa32181d135e99cbb
John <christopherjohnosullivan@gmail.com>
2019-03-30 12:41:27 +0000

    HTML tags create populated scenesArray

--- canvas.js
+++ canvas.js
@@ -4,31 +4,56 @@ canvas.width = 600;
 canvas.height = 400;
 
 var objects = [];
+var scenesArray = [];
 
-function load(){
-    var sprites = document.getElementById("sprites");
-    var frames = sprites.getElementsByTagName("img");
-
+function load() {
+    // Collect all info from HTML
+    var scenes = document.getElementsByTagName("scene");
+    for (var i = 0; i < scenes.length; i++) {
+        var scene = {};
+        var beatsArray = [];
+        scene.type = scenes[i].getAttribute("type")
+        var beats = scenes[i].getElementsByTagName("beat");
+        for (var i = 0; i < beats.length; i++) {
+            var beat = {};
+            var objectsArray = [];
+            beat.frameLength = beats[i].getAttribute("frameLength");
+            var objects = beats[i].getElementsByTagName("object");
+            for (var i = 0; i < objects.length; i++) {
+                var object = {};
+                object.x = objects[i].getAttribute("x");
+                object.y = objects[i].getAttribute("y");
+                var sprites = objects[i].getElementsByTagName("sprite")
+                object.sprites = sprites;
+                objectsArray.push(object)
+            }
+            beat.objects = objectsArray;
+            beatsArray.push(beat)
+        }
+        scene.beats = beatsArray;
+        scenesArray.push(scene)
+    }
+    console.log(scenesArray)
 }
 
-function nextScene(){
+function nextScene() {
     // Initiate Scene
-    
+
     // Run Scene
-    if (true){clip()}
-    if (false){level()}
+    if (true) { clip() }
+    if (false) { level() }
 }
 
-function clip(){
+function clip() {
     // Stays on clip until a change happens
-    if (true){clip}
-    else{nextScene}
+    if (true) { clip }
+    else { nextScene }
 }
 
-function level(){
+function level() {
     // Stays on level until a change happens
-    if (true){level}
-    else{nextScene}
+    if (true) { level }
+    else { nextScene }
 }
 
 
@@ -44,3 +69,4 @@ function render(objects) {
 }
 
 nextScene();
+load()
